7b32330648d1c32b52e31b405e1c0b2b3e754126
Ahmed Gaber <21stlimitless@gmail.com>
2019-06-05 20:18:19 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,17 +1,47 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner_name = ""
+  data[season].each do |bachelorette|
+    if bachelorette["status"] == "Winner"
+      winner_name = bachelorette["name"]
+    end
+  end
+  winner_name.split.first
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  contestant_name = ""
+  data.each do |season, descriptor|
+    data[season].each do |bachelorette|
+      if bachelorette["occupation"] == occupation
+        contestant_name = bachelorette["name"]
+      end
+    end
+  end
+  contestant_name
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  hometown_count = 0
+  data.each do |season, descriptor|
+    data[season].each do |bachelorette|
+      if bachelorette["hometown"] == hometown
+        hometown_count += 1
+      end
+    end
+  end
+  hometown_count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  occupation_data = ""
+  data.each do |season, descriptor|
+    data[season].each do |bachelorette|
+      if bachelorette["hometown"] == hometown
+        occupation_data = bachelorette["occupation"]
+      end
+    end
+  end
+  occupation_data
 end
 
 def get_average_age_for_season(data, season)
